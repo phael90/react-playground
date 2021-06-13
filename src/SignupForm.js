53993624a0d92bba1ed916d62789229771d1921b
phael <raphaelugha@gmail.com>
2021-06-13 19:48:41 +0100

    feat(sign up form):fealds added

--- src/SignupForm.js
+++ src/SignupForm.js
@@ -7,6 +7,9 @@ export const SignupForm = () => {
   const formik = useFormik({
     initialValues: {
       email: "",
+      firstName: "",
+      lastName: "",
+      password: "",
     },
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
@@ -22,7 +25,30 @@ export const SignupForm = () => {
         onChange={formik.handleChange}
         value={formik.values.email}
       />
-
+      <label html="firstName">First name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
+      <label htmlFor="password">Create a Password</label>
+      <input
+        id="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        value={formik.values.password}
+      />
       <button type="submit">Submit</button>
     </form>
   );
