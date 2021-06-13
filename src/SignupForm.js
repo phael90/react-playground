1441543f61b401d779b6570d6a1ceda4c2c1b051
phael <raphaelugha@gmail.com>
2021-06-13 20:11:01 +0100

    feat(sign up form):email validation

--- src/SignupForm.js
+++ src/SignupForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export const SignupForm = () => {
   // Pass the useFormik() hook initial form values and a submit function that will
@@ -11,44 +12,58 @@ export const SignupForm = () => {
       lastName: "",
       password: "",
     },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email address").required("Required"),
+    }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
   });
   return (
     <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      <label html="firstName">First name</label>
-      <input
-        id="firstName"
-        name="firstName"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.firstName}
-      />
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        id="lastName"
-        name="lastName"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.lastName}
-      />
-      <label htmlFor="password">Create a Password</label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        onChange={formik.handleChange}
-        value={formik.values.password}
-      />
+      <div id="sign-up-form-email">
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          // type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div>{formik.errors.email}</div>
+        ) : null}
+      </div>
+      <div id="sign-up-form-firstName">
+        <label html="firstName">First name</label>
+        <input
+          id="firstName"
+          name="firstName"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.firstName}
+        />
+      </div>
+      <div id="sign-up-form-lastName">
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          id="lastName"
+          name="lastName"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.lastName}
+        />
+      </div>
+      <div id="sign-up-form-password">
+        <label htmlFor="password">Create a Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+      </div>
       <button type="submit">Submit</button>
     </form>
   );
