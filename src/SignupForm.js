ad7fe756f365e26dd9e5a40fffb05602725c80c9
phael <raphaelugha@gmail.com>
2021-06-17 05:51:44 +0100

    (sign up form):add validation for: name, surname, password

--- src/SignupForm.js
+++ src/SignupForm.js
@@ -13,7 +13,23 @@ export const SignupForm = () => {
       password: "",
     },
     validationSchema: Yup.object({
-      email: Yup.string().email("Invalid email address").required("Required"),
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Please Enter your email"),
+      firstName: Yup.string()
+        .matches(/^[A-Za-z ]*$/, "Invalid name")
+        .max(30, "Name is too Long!")
+        .required("Please Enter your first name"),
+      lastName: Yup.string()
+        .matches(/^[A-Za-z ]*$/, "Please enter valid name")
+        .max(30, "Last name is too Long!")
+        .required("Please Enter your last name"),
+      password: Yup.string()
+        .matches(
+          /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+          "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+        )
+        .required("Please Enter your password"),
     }),
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
@@ -26,7 +42,7 @@ export const SignupForm = () => {
         <input
           id="email"
           name="email"
-          // type="email"
+          type="email"
           onChange={formik.handleChange}
           value={formik.values.email}
         />
@@ -35,7 +51,7 @@ export const SignupForm = () => {
         ) : null}
       </div>
       <div id="sign-up-form-firstName">
-        <label html="firstName">First name</label>
+        <label html="firstName">First Name</label>
         <input
           id="firstName"
           name="firstName"
@@ -43,6 +59,9 @@ export const SignupForm = () => {
           onChange={formik.handleChange}
           value={formik.values.firstName}
         />
+        {formik.touched.firstName && formik.errors.firstName ? (
+          <div>{formik.errors.firstName}</div>
+        ) : null}
       </div>
       <div id="sign-up-form-lastName">
         <label htmlFor="lastName">Last Name</label>
@@ -53,6 +72,9 @@ export const SignupForm = () => {
           onChange={formik.handleChange}
           value={formik.values.lastName}
         />
+        {formik.touched.lastName && formik.errors.lastName ? (
+          <div>{formik.errors.lastName}</div>
+        ) : null}
       </div>
       <div id="sign-up-form-password">
         <label htmlFor="password">Create a Password</label>
@@ -63,6 +85,9 @@ export const SignupForm = () => {
           onChange={formik.handleChange}
           value={formik.values.password}
         />
+        {formik.touched.password && formik.errors.password ? (
+          <div>{formik.errors.password}</div>
+        ) : null}
       </div>
       <button type="submit">Submit</button>
     </form>
